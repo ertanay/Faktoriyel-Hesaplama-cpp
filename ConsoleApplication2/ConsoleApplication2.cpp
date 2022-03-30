b70803e026de0227874df4451bf1c9a6ec139fe0
ertanay <ertanay8@gmail.com>
2022-03-30 15:38:45 +0300

    Faktöriyel Hesaplama
    C++ ile Faktöriyel hesaplama

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	double n, faktoriyel, i;
+	faktoriyel = 1;
+	cout << "Faktoriyel alınacak değeri giriniz: ";
+	cin >> n;
+	for (i=1; i<=n; i+=1)
+	{
+		faktoriyel = faktoriyel * i;
+	}
+	cout << "Faktoriyel " << faktoriyel << endl;
+}
